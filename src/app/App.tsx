20156c2f72ac3654de5a826077a977d3adf34aa4
Volodymyr Pavlyk <volodymyr.pavlyk@smarttek.solutions>
2023-09-24 20:47:37 +0300

    theme switcher and custom button component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,17 @@
-import { Suspense } from "react";
-import { Link, Route, Routes } from "react-router-dom";
-
 import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "app/providers/ThemeProvider";
+
 import "./styles/index.scss";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
+import { useTheme } from "./providers/ThemeProvider";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
-
+  const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <button onClick={toggleTheme}>Toggle theme</button>
     </div>
   );
 };
---
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./ThemeSwitcher.module.scss";
+import { Theme, useTheme } from "app/providers/ThemeProvider";
+import LigtIcon from "shared/assets/icons/theme-light.svg";
+import DarkIcon from "shared/assets/icons/theme-dark.svg";
+import Button, { ThemeButton } from "shared/ui/Button/Button";
+
+export interface ThemeSwitcherProps {
+  className?: string;
+}
+const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button
+      theme={ThemeButton.CLEAR}
+      className={classNames(cls.ThemeSwitcher, {}, [className])}
+      onClick={toggleTheme}
+    >
+      {theme === Theme.DARK ? <DarkIcon /> : <LigtIcon />}
+    </Button>
+  );
+};
+
+export default ThemeSwitcher;

a4bd9deb118aa489c9d45e9c346875600887189a
Volodymyr Pavlyk <volodymyr.pavlyk@smarttek.solutions>
2023-09-24 18:29:32 +0300

    refactor app.tsx, create widget Navbar and AppLink component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,21 +6,17 @@ import { useTheme } from "app/providers/ThemeProvider";
 import "./styles/index.scss";
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toggleTheme}>Toggle theme</button>
-      <Link to={"/"}>Main</Link>
-      <Link to={"/about"}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
     </div>
   );
 };
